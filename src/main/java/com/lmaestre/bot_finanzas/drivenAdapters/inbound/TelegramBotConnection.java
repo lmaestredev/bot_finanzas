271a72af24183117bccd28a5339b534a1c28b557
luigi <luisaugustomaestre@gmail.com>
2025-01-04 09:09:48 -0300

    feat: Enable Telegram bot connection and refactor configs

--- src/main/java/com/lmaestre/bot_finanzas/drivenAdapters/inbound/TelegramBotConnection.java
+++ src/main/java/com/lmaestre/bot_finanzas/drivenAdapters/inbound/TelegramBotConnection.java
@@ -1,22 +1,21 @@
-//package com.lmaestre.bot_finanzas.drivenAdapters.inbound;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//import org.telegram.telegrambots.meta.TelegramBotsApi;
-//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-//import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-//
-//@Component
-//public class TelegramBotConnection implements CommandLineRunner {
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        try {
-//            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            botsApi.registerBot(new TelegramConfig());
-//        } catch (TelegramApiException e) {
-//            System.out.println(" es aqui........");
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package com.lmaestre.bot_finanzas.drivenAdapters.inbound;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class TelegramBotConnection implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TelegramConfig());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
